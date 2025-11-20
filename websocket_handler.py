4ecf58bd29c3420939d5e0c23e7c7e197a5ce74d
Alvaro <48132162+alvarolaserna@users.noreply.github.com>
2025-11-20 09:49:08 +0100

    Update websocket_handler.py

--- websocket_handler.py
+++ websocket_handler.py
@@ -161,7 +161,7 @@ async def connect_to_backend(uri):
         await asyncio.sleep(10)
 
 async def main_connect_ws():
-    backend_uri = ('wss://stg-lalama.tdlbox.com/ws/'+ os.getenv("BACKEND_WS_URI", "default_client_id"))
+    backend_uri = ('wss://chat.barkoagent.com/ws/'+ os.getenv("BACKEND_WS_URI", "default_client_id"))
     if not backend_uri.startswith("ws://") and not backend_uri.startswith("wss://"):
         logging.error(f"Invalid BACKEND_WS_URI: {backend_uri}. It must start with ws:// or wss://")
         return
